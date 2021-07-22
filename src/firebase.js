30b9dc1490e95c497c1534fc62ff58554ddf2e52
MostafGom <mostafa.suleiman222@gmail.com>
2021-07-22 20:32:32 +0200

    Front End completed you only need to provide==>> your Stripe Public key in App.js/firebase config in firebase.js/your google cloud firebase api end point in axios.js file ----- In Backend in functions folders you need to provide SECRET KEY from STRIPE account in functions/index.js file

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBqJP5LQrXVMNUEDVONP4Xv18jRA8rTTKQ",
-  authDomain: "shop-clone-3a028.firebaseapp.com",
-  projectId: "shop-clone-3a028",
-  storageBucket: "shop-clone-3a028.appspot.com",
-  messagingSenderId: "126721070310",
-  appId: "1:126721070310:web:e3071902dc087d325d9727",
-  measurementId: "G-VFVX277GFE"
+  // Firebase config credentials
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
